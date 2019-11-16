72c748858dd41a5e3af2d809995ad1738c422ab0
David Kayser <davidjkayser@gmail.com>
2019-11-16 13:27:08 -0800

    Convert to MVC Design Patter.

---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by David Kayser on 11/16/19.
+//  Copyright © 2019 David Kayser. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
